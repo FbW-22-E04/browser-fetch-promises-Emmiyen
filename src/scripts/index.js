c21813d5513555f554664c529f1cf74dbb9a63c5
Danica Yener <ny21037@googlemail.com>
2022-09-28 14:13:59 +0200

    browser fetch

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,15 @@
 // The following line makes sure your styles are included in the project. Don't remove this.
+// import '../styles/main.scss';
 import '../styles/main.scss';
 // Import any additional modules you want to include below \/
 
 
 // \/ All of your javascript should go here \/
+const modal = document.querySelector('.modal')
+
+const popup = new Promise((resolve, reject)=>{
+setTimeout(()=>{
+modal.style.display = 'block'
+},6000)
+})
 
